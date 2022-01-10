710413fb851063c75bf382a97f3bec55f4d50edb
Abdellah Targaoui <abdellah.targaoui@gmail.com>
2022-01-10 13:25:27 +0100

    orgnise template

--- pages/urls.py
+++ pages/urls.py
@@ -20,4 +20,9 @@ from pages import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('cars', views.cars, name='cars'),
+    path('contact', views.contact, name='contact'),
+
 ]
